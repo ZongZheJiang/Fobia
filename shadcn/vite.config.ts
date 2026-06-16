4b3cd670798a0a4369d210ddb3f22869ddbf3d4f
jiangzongzhe <jiangzongzhe374@gmail.com>
2026-06-16 17:22:39 -0400

    integrating @vercel/react-router

--- shadcn/vite.config.ts
+++ shadcn/vite.config.ts
@@ -2,13 +2,15 @@
 
 import { defineConfig } from 'vite'
 
+import { vercelPreset } from '@vercel/react-router/vite';
+
 import { reactRouter } from "@react-router/dev/vite"
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss()],
+  plugins: [reactRouter(), tailwindcss(), vercelPreset()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
