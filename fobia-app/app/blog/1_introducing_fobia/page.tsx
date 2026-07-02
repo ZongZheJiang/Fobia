d622a0af9799ecc2ff3b7b56629aa51aff47e829
jiangzongzhe <jiangzongzhe374@gmail.com>
2026-07-02 11:12:32 -0400

    added blank page for first blogpost

---
+++ fobia-app/app/blog/1_introducing_fobia/page.tsx
@@ -0,0 +1,14 @@
+import "../../index.css"
+
+import { NavigationMenuDemo } from "@/examples/navigation-menu"
+
+function Blog_1_Introducing_Fobia() {
+    return (
+        <div>
+            <NavigationMenuDemo />
+            <h2>Hello World</h2>
+        </div>
+    )
+}
+
+export default Blog_1_Introducing_Fobia
